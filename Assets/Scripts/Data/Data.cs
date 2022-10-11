9ae81a8dfe38c945001f936161e224a131ad30ad
Farawa <dzipopo@mail.ru>
2022-10-11 03:06:13 +0300

    spawn steps

--- Assets/Scripts/Data/Data.cs
+++ Assets/Scripts/Data/Data.cs
@@ -5,5 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data", menuName = "Scriptables/Data", order = 1)]
 public class Data : ScriptableObject
 {
-
+    public Vector2Int stepsRange;
+    public Vector3 stepOffset;
+    public Vector3 stepSize;
+    public GameObject stepPrefab;
 }
--- Assets/Scripts/StairsController.cs
+++ Assets/Scripts/StairsController.cs
@@ -5,5 +5,18 @@ using UnityEngine;
 public class StairsController : MonoBehaviour
 {
     [SerializeField] private Transform player;
-    
+    [SerializeField] private Transform stepsParent;
+    private Transform[] steps;
+
+    private void Start()
+    {
+        var stepsCount = DataManager.Data.stepsRange.x + DataManager.Data.stepsRange.y + 1;
+        steps = new Transform[stepsCount];
+        for (int i = 0; i < stepsCount; i++)
+        {
+            var step = Instantiate(DataManager.Data.stepPrefab, stepsParent);
+            step.transform.localScale = DataManager.Data.stepSize;
+            steps[i] = step.transform;
+        }
+    }
 }
